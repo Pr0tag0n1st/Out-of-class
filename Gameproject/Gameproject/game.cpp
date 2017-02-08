6767965cdc61635f6573e864e46f89261424fd34
Game110AM <Game110AM@605-110-STU03>
2017-02-08 09:10:15 -0700

    slight changes

--- Gameproject/Gameproject/game.cpp
+++ Gameproject/Gameproject/game.cpp
@@ -26,16 +26,18 @@ int main() {
 			else if (input == 'w')
 				room = 3;
 			break;
-		case 2: if (weapon == 0){
+		case 2: if (weapon == 0) {
 			cout << "You arrived in a room with a pedestal holding a sword.  Will you draw it from its prison?" << endl;
 			cin >> input;
 			if (input == 'y') {
 				cout << "You held the sword in your hand.  You quickly grew accustomed to its weight." << endl;
 				weapon = 1;
 			}
-			else if (weapon == 1){
+			else if (weapon == 1) {
 				cout << "The pedestal where you found your sword is empty.  The rest of the room is dimly lit, and nothing is visible." << endl;
-				cout<<"There are two more paths, one to the "
+				cout << "There are two more paths, one to the "
+			}
+		}
 		}
 	}
 }
