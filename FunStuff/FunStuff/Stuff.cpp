eba7a54b99f368c09064deeb0565aaaff8784c56
TJ <665825@dpsk12.net>
2017-02-07 21:03:06 -0700

    All sorts of stuff

---
+++ FunStuff/FunStuff/Stuff.cpp
@@ -0,0 +1,12 @@
+#/*include <iostream>
+using namespace std;
+int main() {
+	int stars = 0;
+	cout << "Hello!  How many stars would you like to see?" << endl;
+	cin >> stars;
+	cout << "Ok!  Here you go!" << endl;
+	for (int i = 0; i < stars; i++) {
+		cout << "*";
+	}
+	cout << "" << endl;
+}*/
