5d515563ae733aafa24c3d74e5285514f985f346
Game110AM <Game110AM@605-110-STU03>
2017-02-09 10:44:07 -0700

    save

--- Gameproject/Gameproject/game.cpp
+++ Gameproject/Gameproject/game.cpp
@@ -22,18 +22,35 @@ int main() {
 			else if (input == 'w')
 				room = 3;
 			break;
-		case 2: if (weapon == 0) {
-			cout << "You arrived in a room with a pedestal holding a sword.  Will you draw it from its prison?" << endl;
+		case 2: 
+			cout << "You arrived in a room with a pedestal." << endl;
+			if (weapon == 0)
+				cout << "There is a sword in the pedestal.  Will you take it?" << endl;
 			cin >> input;
 			if (input == 'y') {
-				cout << "You held the sword in your hand.  You quickly grew accustomed to its weight." << endl;
+				cout << "You took the sword with you." << endl;
 				weapon = 1;
 			}
-			else if (weapon == 1) {
-				cout << "The pedestal where you found your sword is empty.  The rest of the room is dimly lit, and nothing is visible." << endl;
-				cout << "There are two more paths, one to the "
+			else if (input == 'n') {
+				cout << "You left the sword alone." << endl;
 			}
-		}
+			else if (weapon == 1) {
+				cout << "There are two paths you can take, one to the west and the other to the south." << endl;
+				cout << "Which way will you go?" << endl;
+				cin >> input;
+				if (input == 'w')
+					room = 1;
+				else if (input == 's')
+					room = 4;
+			}
+			break;
+			
+		case 3:
+			cout << "There is nothing to do here.  Go back east." << endl;
+			cin >> input;
+			if (input == 'e')
+				room = 1;
+			break;
 		}
 	}
 }
